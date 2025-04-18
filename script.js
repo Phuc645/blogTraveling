1fe8b99a5498e92a1173dfa5debe00ec887945f6
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-04-18 16:00:30 +0700

    Enable script,js

---
+++ script.js
@@ -0,0 +1,22 @@
+const posts = [
+    { name: "Vịnh Hạ Long", image: "images/halong.jpg", alt: "Vịnh Hạ Long", link: "pages/vinhhalong.html" },
+    { name: "Hội An", image: "images/hoian.jpg", alt: "Hội An", link: "pages/hoian.html" },
+    { name: "Phú Quốc", image: "images/phuquoc.jpg", alt: "Phú Quốc", link: "pages/vinhhalong.html" }
+];
+
+function render() {
+    const content = document.querySelector(".content");
+    posts.forEach(post => {
+        const card = document.createElement("div");
+        card.className = "card";
+        let html = `<a href="${post.link}">
+                <img src="${post.image}" class="tourist" alt="${post.alt}"/>
+                <h3>${post.name}</h3>
+                </a>`
+        card.innerHTML = html;
+        content.appendChild(card);
+    });
+}
+document.addEventListener("DOMContentLoaded", render);
+
+
