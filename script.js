785f35b52f42dcb8537d5f3ade872dade7e06792
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-05-06 10:34:05 +0700

    Add JSON

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const posts = [
-    { name: "Vịnh Hạ Long", image: "images/halong.jpg", alt: "Vịnh Hạ Long", link: "pages/vinhhalong.html" },
-    { name: "Hội An", image: "images/hoian.jpg", alt: "Hội An", link: "pages/hoian.html" },
-    { name: "Phú Quốc", image: "images/phuquoc.jpg", alt: "Phú Quốc", link: "pages/phuquoc.html" }
-];
 
-function renderpost() {
+
+fetch("posts.json").then(response => response.json()).then(posts => renderpost(posts));
+
+function renderpost(posts) {
     const content = document.querySelector(".content");
     posts.forEach(post => {
         const card = document.createElement("div");
